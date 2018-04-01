ac7037c1c61c466ca4a4258aa39e445f560aec84
Ian Nastajus <ian.nastajus@gmail.com>
2018-04-01 11:12:02 -0400

    Article.sync().then(function() { ... }); works

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ let Article = connection.define('article', {
 	//updatedAt
 });
 
-connection.sync();
+//connection.sync();
 	//1. will connect to database
 	//2. will generate the SQL that creates corresponding table in db
+
+connection.sync().then(function() {
+	Article.create({
+		title: 'demo title',
+		body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	})
+});
+	//* since sync might take a long time, this must be designed to insert via callback.
